4c1b63287b01e9d8d58f669e9d93dbb0a6393678
Luka Ravnikar <lukaravnikar@Lukas-MacBook-Air.local>
2025-03-11 14:17:20 +0100

    Fixed Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Use this if your GitHub repository is named "lukagrunt.com"
+  base: '/lukagrunt.com/',  // Make sure the base path is correct for GitHub Pages
+
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
