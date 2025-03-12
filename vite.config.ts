a988bd65f13cc00c32e1aba77699a225ed931205
LukaGrunt <lukagrunt@gmail.com>
2025-03-12 18:57:07 +0100

    Update vite config base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://lukagrunt.com/',
+  base: '/', // Using root path since we're using a custom domain
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
